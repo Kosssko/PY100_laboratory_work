992c2b93628452ddac3ad3162b7f0601671b0383
Kossster <115177455+Kossster@users.noreply.github.com>
2022-10-23 21:06:54 +0300

    Add files via upload
    Лаб(Тема_3)

---
+++ Лабораторная работа(Тема 3)/main_task_6.py
@@ -0,0 +1,9 @@
+list_numbers = [2, -93, -2, 8, -36, -44, -1, -85, -14, 90, -22, -90, -100, -8, 38, -92, -45, 67, 53, 25]
+
+max_index = max(list_numbers)
+last_index = 25
+list_numbers[19] = max_index
+list_numbers[9] = last_index
+
+# print(len(list_numbers)) = 20, не 10 как в задании.
+print(list_numbers)
